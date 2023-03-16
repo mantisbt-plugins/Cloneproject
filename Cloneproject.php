84fe588fcbab95817a14fdcea0565a6efb6b249f
CasN <cas@nuy.info>
2023-03-16 16:24:14 +0100

    Update Cloneproject.php

--- Cloneproject.php
+++ Cloneproject.php
@@ -8,8 +8,8 @@ class CloneprojectPlugin extends MantisPlugin {
         $this->requires    = array('MantisCore'       => '2.0.0',);
         $this->author      = 'Cas Nuy';
         $this->contact     = 'Cas.Nuy@stahl.com';
-        $this->url         = 'http://www.nuy.info/mantis2';
-		$this->page			= 'config';
+        $this->url         = 'https://github.com/mantisbt-plugins/Cloneproject2';
+	$this->page	   = 'config';
     }
 	
 	function config() {
